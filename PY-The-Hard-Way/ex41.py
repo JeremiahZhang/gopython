9bc9430b4a45119da149ebf8133c9f2021eaa758
JeremiahZhang <z.l12@mail.scut.edu.cn>
2017-04-05 21:10:04 +0800

    ex41.py modify

--- PY-The-Hard-Way/ex41.py
+++ PY-The-Hard-Way/ex41.py
@@ -3,3 +3,70 @@
 # read codes 
 
 # using print to understand.
+
+# word Drill
+
+"""
+
+1. class: 
+Tell python to make a new type of thing.
+
+2. object: 
+Two meanings: 
+- the most basic type of thing, and 
+- any instance of some thing.
+
+3. instance: 
+What you get when you tell Python to create a class
+
+4. def
+How you define a function inside a class.
+
+5. self
+Inside the functions in a class, self is a variable
+for the instance/object being accessed.
+
+6. inheritance
+The concept that one class can inheirt traits from 
+another class, much like you and your parents.
+
+7. composition
+The concept that a class can be composed of other classes
+as parts, much like how a car has wheel.
+
+8. attribute
+A property classes have that are from composition and are
+usually variables.
+
+9. is-a
+A phrase to say that something inherits from another, as
+in a "salmon" is-a "fish."
+
+10. has-a
+A phrase to say that something is composed of other things or
+has a trait, as in "a salmon has-a mouth."
+"""
+
+# Phrase Drills
+
+"""
+
+1. class X(Y):
+"make a class named X that is-a Y"
+
+2. class X(object): def __init__(self, J)
+"Class X has-a __init__ that takes self and J parameters"
+
+3. class X(object): def M(self, J)
+"Class X has-a function named M that takes self and J parameters"
+
+4. foo = X()
+"Set foo to an instance of class X."
+
+5. foo.M(J)
+"From foo get the M function, and call it with parameters self, J."
+
+6. foo.K = Q
+"From foo get the K attribute and set it to Q"
+
+"""
