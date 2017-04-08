2be72a2bd42b8c02d37519ee327e67b28b379698
JeremiahZhang <zhangleisuda@gmail.com>
2017-04-08 19:31:26 +0800

    first test ex49

---
+++ PY-The-Hard-Way/ex48/skeleton/ex49_first.py
@@ -0,0 +1,9 @@
+from ex48 import lexicon
+
+print lexicon.scan('go north')
+
+print lexicon.scan('kill the princess')
+
+print lexicon.scan('eat the bear')
+
+print lexicon.scan("open the door and smack the bear in the nose")
