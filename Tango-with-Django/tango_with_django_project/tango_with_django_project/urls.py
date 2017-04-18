b994fa68b501af476ef898dfa639a884907b378b
JeremiahZhang <z.l12@mail.scut.edu.cn>
2017-04-18 09:41:16 +0800

    :dancer: Django rango create a view

--- Tango-with-Django/tango_with_django_project/tango_with_django_project/urls.py
+++ Tango-with-Django/tango_with_django_project/tango_with_django_project/urls.py
@@ -16,6 +16,9 @@ Including another URLconf
 from django.conf.urls import url
 from django.contrib import admin
 
+from rango import views
+
 urlpatterns = [
+    url(r'^$', views.index, name = 'index'),
     url(r'^admin/', admin.site.urls),
 ]
