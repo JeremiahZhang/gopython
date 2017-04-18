539844b8c0494747f52d82180a3373dad1e7d452
JeremiahZhang <z.l12@mail.scut.edu.cn>
2017-04-18 10:13:18 +0800

    :dancer: django rango mapping urls

--- Tango-with-Django/tango_with_django_project/tango_with_django_project/urls.py
+++ Tango-with-Django/tango_with_django_project/tango_with_django_project/urls.py
@@ -15,10 +15,15 @@ Including another URLconf
 """
 from django.conf.urls import url
 from django.contrib import admin
+from django.conf.urls import include
 
 from rango import views
 
 urlpatterns = [
     url(r'^$', views.index, name = 'index'),
+    url(r'^rango/', include('rango.urls')),
+    # above maps any URLs starting
+    # with rango/ to be handled by
+    # the rango application
     url(r'^admin/', admin.site.urls),
 ]
