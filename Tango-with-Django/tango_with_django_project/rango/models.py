6c92a6815cc29b10b8e303274a78253a0085bab9
JeremiahZhang <z.l12@mail.scut.edu.cn>
2017-04-19 15:24:57 +0800

    :clap: :dancer: Django models and database

--- Tango-with-Django/tango_with_django_project/rango/models.py
+++ Tango-with-Django/tango_with_django_project/rango/models.py
@@ -3,3 +3,18 @@ from __future__ import unicode_literals
 from django.db import models
 
 # Create your models here.
+
+class Category(models.Model):
+    name = models.CharField(max_length=128, unique=True)
+    
+    def __unicode__(self): # I use Python 2.x
+        return self.name
+
+class Page(models.Model):
+    category = models.ForeignKey(Category)
+    title = models.CharField(max_length=128)
+    url = models.URLField()
+    views = models.IntegerField(default=0)
+    
+    def __unicode__(self):
+        return self.title
