18f646252ef6ad8d33cff741b59663f68f9f08de
JeremiahZhang <z.l12@mail.scut.edu.cn>
2017-04-24 13:09:58 +0800

    :dancer: twd c5 ex 01 views likes in catagory

--- Tango-with-Django/tango_with_django_project/rango/models.py
+++ Tango-with-Django/tango_with_django_project/rango/models.py
@@ -6,7 +6,9 @@ from django.db import models
 
 class Category(models.Model):
     name = models.CharField(max_length=128, unique=True)
-    
+    views = models.IntegerField(default=0)
+    likes = models.IntegerField(default=0)
+
     def __unicode__(self): # I use Python 2.x
         return self.name
 
@@ -15,6 +17,6 @@ class Page(models.Model):
     title = models.CharField(max_length=128)
     url = models.URLField()
     views = models.IntegerField(default=0)
-    
+
     def __unicode__(self):
         return self.title
