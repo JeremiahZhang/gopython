1830a245010426d72f3fb2060ee4897cb1a6bb2c
JeremiahZhang <z.l12@mail.scut.edu.cn>
2017-04-24 13:28:14 +0800

    twd c5 ex update population script

--- Tango-with-Django/tango_with_django_project/populate_rango.py
+++ Tango-with-Django/tango_with_django_project/populate_rango.py
@@ -43,9 +43,9 @@ def populate():
          "url": "http://jinja.pocoo.org/"}
     ]
     
-    cats = {"Python": {"pages": python_pages},
-            "Django": {"pages": django_pages},
-            "Other Frameworks": {"pages": other_pages}
+    cats = {"Python": {"pages": python_pages, "views": 128, "likes": 64},
+            "Django": {"pages": django_pages, "views": 64, "likes": 32},
+            "Other Frameworks": {"pages": other_pages, "views": 32, "likes": 16}
     }
 
     # if you want to add more categories or pages,
@@ -58,7 +58,7 @@ def populate():
     # for more information about how to iterate over a dictionary properly.
     
     for cat, cat_data in cats.items():
-        c = add_cat(cat)
+        c = add_cat(cat, cat_data["views"], cat_data["likes"])
         for p in cat_data["pages"]:
             add_page(c, p["title"], p["url"])
     
@@ -76,8 +76,10 @@ def add_page(cat, title, url, views=0):
 
 # get_or_create
 # https://docs.djangoproject.com/en/1.10/ref/models/querysets/#get-or-create
-def add_cat(name):
+def add_cat(name, views=0, likes=0):
     c = Category.objects.get_or_create(name=name)[0]
+    c.views = views
+    c.likes = likes
     c.save()
     return c # return one example- Category: Python
 
