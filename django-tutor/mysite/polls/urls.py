fd9bffd98e8ac044d2f51786cc5933e5f52f7692
JeremiahZhang <z.l12@mail.scut.edu.cn>
2017-04-25 18:16:01 +0800

    namespacing URL names

--- django-tutor/mysite/polls/urls.py
+++ django-tutor/mysite/polls/urls.py
@@ -2,12 +2,14 @@ from django.conf.urls import url
 
 from . import views
 
+app_name = 'polls'
+
 urlpatterns = [
     # ex: /polls/
     url(r'^$', views.index, name='index'),
     # ex: /polls/5/
     # the 'name' value as called by the {% url %} template tag
-    url(r'^specifics/(?P<question_id>[0-9]+)/$', views.detail, name='detail'),
+    url(r'^(?P<question_id>[0-9]+)/$', views.detail, name='detail'),
     # ex: /polls/5/results/
     url(r'^(?P<question_id>[0-9]+)/results/$', views.results, name='results'),
     # ex: /polls/5/vote/
