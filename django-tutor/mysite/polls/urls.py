89a2f66fd5777a248ac06838ffbb5c84fae8f472
JeremiahZhang <z.l12@mail.scut.edu.cn>
2017-04-25 18:09:53 +0800

    removeing hardcoded URLs in templates

--- django-tutor/mysite/polls/urls.py
+++ django-tutor/mysite/polls/urls.py
@@ -6,6 +6,7 @@ urlpatterns = [
     # ex: /polls/
     url(r'^$', views.index, name='index'),
     # ex: /polls/5/
+    # the 'name' value as called by the {% url %} template tag
     url(r'^(?P<question_id>[0-9]+)/$', views.detail, name='detail'),
     # ex: /polls/5/results/
     url(r'^(?P<question_id>[0-9]+)/results/$', views.results, name='results'),
