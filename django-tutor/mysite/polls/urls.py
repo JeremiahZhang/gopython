4b284e7f941b00f1b784056cd2e259464a5bab28
JeremiahZhang <z.l12@mail.scut.edu.cn>
2017-04-25 18:12:40 +0800

    add any word is ok now

--- django-tutor/mysite/polls/urls.py
+++ django-tutor/mysite/polls/urls.py
@@ -7,7 +7,7 @@ urlpatterns = [
     url(r'^$', views.index, name='index'),
     # ex: /polls/5/
     # the 'name' value as called by the {% url %} template tag
-    url(r'^(?P<question_id>[0-9]+)/$', views.detail, name='detail'),
+    url(r'^specifics/(?P<question_id>[0-9]+)/$', views.detail, name='detail'),
     # ex: /polls/5/results/
     url(r'^(?P<question_id>[0-9]+)/results/$', views.results, name='results'),
     # ex: /polls/5/vote/
