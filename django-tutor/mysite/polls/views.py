6e548b3f5ae9b686affff27e242545cb8f753aca
JeremiahZhang <z.l12@mail.scut.edu.cn>
2017-04-25 16:47:51 +0800

    shortcut render()

--- django-tutor/mysite/polls/views.py
+++ django-tutor/mysite/polls/views.py
@@ -9,12 +9,13 @@ from .models import Question
 
 def index(request): # display the lastest few question
     lastest_question_list = Question.objects.order_by('-pub_date')[:5]
-    template = loader.get_template('polls/index.html') # direct load from polls/templates/
+    # template = loader.get_template('polls/index.html') # direct load from polls/templates/
     context = {
         'latest_question_list': lastest_question_list,
     } # context transfer to polls/index.html then show the index page in polls/
     # output = ', '.join([q.question_text for q in lastest_question_list])
-    return HttpResponse(template.render(context, request))
+    # return HttpResponse(template.render(context, request))
+    return render(request, 'polls/index.html', context)
 
 def detail(request, question_id): # displays a question text, without results but with a form to vote
     return HttpResponse("You're looking at question %s." % question_id)
