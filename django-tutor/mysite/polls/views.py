80e77c83aee293b65f1ea6b0cc888be8e04e4ff3
JeremiahZhang <z.l12@mail.scut.edu.cn>
2017-04-27 10:45:57 +0800

    testing our new views

--- django-tutor/mysite/polls/views.py
+++ django-tutor/mysite/polls/views.py
@@ -11,6 +11,8 @@ from django.http import HttpResponseRedirect
 from django.urls import reverse
 from django.views import generic
 
+from django.utils import timezone
+
 from .models import Question
 from .models import Choice
 
@@ -48,3 +50,12 @@ def vote(request, question_id): # handles voting for a particular choice in a pa
         # with POST data. This prevents data from being posted twice if a 
         # user hits the Back button.
         return HttpResponseRedirect(reverse('polls:results', args=(question_id,)))
+
+def get_queryset(self):
+    """
+    Return the last five published question (not including those set to be
+    published in the future).
+    """
+    return Question.objects.filter(
+        pub_date__lte=timezone.now()
+        ).order_by('-pub_date')[:5]
