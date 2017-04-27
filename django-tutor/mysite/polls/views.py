91d4f3d16c82962658d0f086943798cf6c5c45a6
JeremiahZhang <z.l12@mail.scut.edu.cn>
2017-04-27 10:54:29 +0800

    test detail views successfully but index view failed

--- django-tutor/mysite/polls/views.py
+++ django-tutor/mysite/polls/views.py
@@ -29,6 +29,12 @@ class DetailView(generic.DetailView):
     model = Question
     template_name = 'polls/detail.html'
 
+    def get_queryset(self):
+        """
+        Excludes any questions that arenot published yet.
+        """
+        return Question.objects.filter(pub_date__lte=timezone.now())
+
 class ResultsView(generic.DetailView):
     model = Question
     template_name = 'polls/results.html'
