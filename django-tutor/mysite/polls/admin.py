eb8aa09851eee92d9970a552d2624c483c6b52d0
JeremiahZhang <z.l12@mail.scut.edu.cn>
2017-04-27 13:35:22 +0800

    'search-fields-at-the-top'

--- django-tutor/mysite/polls/admin.py
+++ django-tutor/mysite/polls/admin.py
@@ -18,6 +18,7 @@ class QuestionAdmin(admin.ModelAdmin):
     # fields = ['pub_date', 'question_text']
     list_display = ('question_text', 'pub_date', 'was_published_recently')
     list_filter = ['pub_date']
-
+    search_fields = ['question_text']
+    
 admin.site.register(Question, QuestionAdmin)
 # admin.site.register(Choice)
