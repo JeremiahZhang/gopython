2006d9251f567f11caf7780d0b84087d4ce8816a
JeremiahZhang <z.l12@mail.scut.edu.cn>
2017-04-27 13:01:21 +0800

    fab local error

--- fabfile.py
+++ fabfile.py
@@ -2,16 +2,8 @@
 
 from fabric.api import local
 
-# fab post: your-post-name
-def post(name):
-    local("./scripts/newpost %s" % name)
-
-# fab tag
-def tag():
-    local("./scripts/generate-tags")
-
 # please use: fab pu:'discribation'
 def pu(discribation):
     local("git ad")
-    local("git ci -m '%s' " % discribation)
-local("git pu")
+    local("git ci '%s' " % discribation)
+    local("git pu")
