0ef02df6c5d62726b8e8d1aae2590e7028a6db80
JeremiahZhang <z.l12@mail.scut.edu.cn>
2017-04-27 13:08:28 +0800

    'fabtest'

--- fabfile.py
+++ fabfile.py
@@ -2,8 +2,8 @@
 
 from fabric.api import local
 
-# please use: fab pu:'discribation'
+# please use: fab pu:discribation
 def pu(discribation):
     local("git ad")
-    local("git ci '%s' " % discribation)
+    local("git commit -m '%s'" % discribation)
     local("git pu")
