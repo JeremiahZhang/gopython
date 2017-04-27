090bf06f7fbf145800364690a82254d8f66d4fda
JeremiahZhang <z.l12@mail.scut.edu.cn>
2017-04-27 08:48:25 +0800

    running test

--- django-tutor/mysite/polls/tests.py
+++ django-tutor/mysite/polls/tests.py
@@ -1,3 +1,19 @@
+import datetime
+
+from django.utils import timezone
 from django.test import TestCase
 
+from .models import Question
+
 # Create your tests here.
+
+class QuestionMethodTests(TestCase):
+    
+    def test_was_published_recently_with_future_question(self):
+        """
+        was_published_recently() should return False for questions whose
+        pub_date is in the future.
+        """
+        time = timezone.now() + datetime.timedelta(days=30)
+        future_question = Question(pub_date=time)
+        self.assertIs(future_question.was_published_recently(), False)
--- django-tutor/test_bug.py
+++ django-tutor/test_bug.py
@@ -1,6 +1,30 @@
+"""
 >>> import datetime
 >>> from django.utils import timezone
 >>> from polls.models import Question
 >>> # create a Question instance with pub_date 30 days in the future
 >>> future_question = Question(pub_date=timezone.now() + datetime.timedelta(days=30))
 >>> future_question.was_published_recently()
+True
+"""
+
+"""
+[rango] PS E:\2016-for_Vocation\gopython\django-tutor\mysite> python manage.py test polls
+Creating test database for alias 'default'...
+F
+======================================================================
+FAIL: test_was_published_recently_with_future_question (polls.tests.QuestionMethodTests)
+----------------------------------------------------------------------
+Traceback (most recent call last):
+  File "E:\2016-for_Vocation\gopython\django-tutor\mysite\polls\tests.py", line 19, in test_was_published_recently_with_
+future_question
+    self.assertIs(future_question.was_published_recently(), False)
+AssertionError: True is not False
+
+----------------------------------------------------------------------
+Ran 1 test in 0.001s
+
+FAILED (failures=1)
+Destroying test database for alias 'default'...
+"""
+
