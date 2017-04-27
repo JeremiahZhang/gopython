cb6c34323323cca5b05f74181fe220e0e05a40e7
JeremiahZhang <z.l12@mail.scut.edu.cn>
2017-04-27 13:20:20 +0800

    'choice-in-Question'

--- django-tutor/mysite/polls/admin.py
+++ django-tutor/mysite/polls/admin.py
@@ -5,12 +5,17 @@ from django.contrib import admin
 from .models import Question
 from .models import Choice
 
+class ChoiceInline(admin.StackedInline):
+    model = Choice
+    extra = 3
+
 class QuestionAdmin(admin.ModelAdmin):
     fieldsets = [
         ('Question infor', {'fields': ['question_text']}),
         ('Date information', {'fields': ['pub_date']}),
     ]
+    inlines = [ChoiceInline]
     # fields = ['pub_date', 'question_text']
 
 admin.site.register(Question, QuestionAdmin)
-admin.site.register(Choice)
+# admin.site.register(Choice)
