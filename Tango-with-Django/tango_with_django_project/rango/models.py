b9b8cd2f254420815c82104ccb7f36303786abe9
JeremiahZhang <z.l12@mail.scut.edu.cn>
2017-04-27 14:54:26 +0800

    'change-str'

--- Tango-with-Django/tango_with_django_project/rango/models.py
+++ Tango-with-Django/tango_with_django_project/rango/models.py
@@ -2,21 +2,25 @@ from __future__ import unicode_literals
 
 from django.db import models
 
+from django.utils.encoding import python_2_unicode_compatible
+
 # Create your models here.
 
+@python_2_unicode_compatible # only if you need to support Python 2
 class Category(models.Model):
     name = models.CharField(max_length=128, unique=True)
     views = models.IntegerField(default=0)
     likes = models.IntegerField(default=0)
 
-    def __unicode__(self): # I use Python 2.x
+    def __str__(self): 
         return self.name
 
+@python_2_unicode_compatible
 class Page(models.Model):
     category = models.ForeignKey(Category)
     title = models.CharField(max_length=128)
     url = models.URLField()
     views = models.IntegerField(default=0)
 
-    def __unicode__(self):
+    def __str__(self):
         return self.title
