dd42416e1602fdb7372f835c8c7c2db7fac5e659
JeremiahZhang <z.l12@mail.scut.edu.cn>
2017-04-27 17:29:36 +0800

    'twd-c5-finial-exercise'

--- Tango-with-Django/tango_with_django_project/rango/admin.py
+++ Tango-with-Django/tango_with_django_project/rango/admin.py
@@ -4,5 +4,9 @@ from django.contrib import admin
 
 from rango.models import Category, Page
 
+class PageAdmin(admin.ModelAdmin):
+    list_display = ('title', 'category', 'url')
+
+
 admin.site.register(Category)
-admin.site.register(Page)
+admin.site.register(Page, PageAdmin)
