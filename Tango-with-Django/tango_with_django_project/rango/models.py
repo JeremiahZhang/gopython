7d2f7cc42279607b958155ef5c08c6b80d01500b
JeremiahZhang <z.l12@mail.scut.edu.cn>
2017-04-28 10:55:31 +0800

    'slug-first-demoe'

--- Tango-with-Django/tango_with_django_project/rango/models.py
+++ Tango-with-Django/tango_with_django_project/rango/models.py
@@ -4,6 +4,8 @@ from django.db import models
 
 from django.utils.encoding import python_2_unicode_compatible
 
+from django.template.defaultfilters import slugify
+
 # Create your models here.
 
 @python_2_unicode_compatible # only if you need to support Python 2
@@ -11,6 +13,14 @@ class Category(models.Model):
     name = models.CharField(max_length=128, unique=True)
     views = models.IntegerField(default=0)
     likes = models.IntegerField(default=0)
+    slug = models.SlugField()
+    
+    def save(self, *args, **kwargs):
+        self.slug = slugify(self.name)
+        super(Category, self).save(*args, **kwargs)
+    
+    class Meta:
+        verbose_name_plural = 'categories'
 
     def __str__(self): 
         return self.name
