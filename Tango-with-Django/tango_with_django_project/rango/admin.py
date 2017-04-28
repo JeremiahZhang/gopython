a87c8e4902132cceb136ce96794c48745ddb4ed4
JeremiahZhang <z.l12@mail.scut.edu.cn>
2017-04-28 11:02:04 +0800

    'auto-add-slug-field'

--- Tango-with-Django/tango_with_django_project/rango/admin.py
+++ Tango-with-Django/tango_with_django_project/rango/admin.py
@@ -4,9 +4,13 @@ from django.contrib import admin
 
 from rango.models import Category, Page
 
+# Add in this class to customise the Admin Interface
+class CategoryAdmin(admin.ModelAdmin):
+    prepopulated_filed = {'slug': ('name',)}
+
 class PageAdmin(admin.ModelAdmin):
     list_display = ('title', 'category', 'url')
 
 
-admin.site.register(Category)
+admin.site.register(Category, CategoryAdmin)
 admin.site.register(Page, PageAdmin)
