aa61772b1a5a0cec8db164f0284e5fb1c94a48aa
JeremiahZhang <z.l12@mail.scut.edu.cn>
2017-05-03 16:12:54 +0800

    'extend-application'

--- djangogirls/djangogirlsite/blog/urls.py
+++ djangogirls/djangogirlsite/blog/urls.py
@@ -3,5 +3,6 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'^$', views.post_list, name='post_list')
+    url(r'^$', views.post_list, name='post_list'), 
+    url(r'^post/(?P<pk>[0-9]+)/$', views.post_detail, name='post_detail'), 
 ]
--- djangogirls/djangogirlsite/blog/views.py
+++ djangogirls/djangogirlsite/blog/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from django.shortcuts import get_object_or_404
 
 # Create your views here.
 from django.utils import timezone
@@ -8,3 +9,7 @@ from .models import Post
 def post_list(request):
     posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
     return render(request, 'blog/post_list.html', {'posts': posts})
+
+def post_detail(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    return render(request, 'blog/post_detail.html', {'post': post})
