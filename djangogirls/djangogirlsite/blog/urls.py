ff444cea286cc8cdec5601e4537def1b6fabc823
JeremiahZhang <z.l12@mail.scut.edu.cn>
2017-05-04 09:28:29 +0800

    'page-with-unpublished-post'

--- djangogirls/djangogirlsite/blog/urls.py
+++ djangogirls/djangogirlsite/blog/urls.py
@@ -7,4 +7,5 @@ urlpatterns = [
     url(r'^post/(?P<pk>[0-9]+)/$', views.post_detail, name='post_detail'), 
     url(r'^post/new/$', views.post_new, name='post_new'),
     url(r'^post/(?P<pk>[0-9]+)/edit/$', views.post_edit, name='post_edit'), 
+    url(r'^draft/$', views.post_draft_list, name='post_draft_list'), 
 ]
--- djangogirls/djangogirlsite/blog/views.py
+++ djangogirls/djangogirlsite/blog/views.py
@@ -23,7 +23,7 @@ def post_new(request):
         if form.is_valid():
             post = form.save(commit=False)
             post.author = request.user
-            post.published_date = timezone.now()
+            # post.published_date = timezone.now()
             post.save()
             return redirect('post_detail', pk=post.pk)
     else:
@@ -37,9 +37,13 @@ def post_edit(request, pk):
         if form.is_valid():
             post = form.save(commit=False)
             post.author = request.user
-            post.published_date = timezone.now()
+            # post.published_date = timezone.now()
             post.save()
             return redirect('post_detail', pk=post.pk)
     else:
         form = PostForm(instance=post)
     return render(request, 'blog/post_edit.html', {'form': form})
+
+def post_draft_list(request):
+    posts = Post.objects.filter(published_date__isnull=True).order_by('created_date')
+    return render(request, 'blog/post_draft_list.html', {'posts': posts})
