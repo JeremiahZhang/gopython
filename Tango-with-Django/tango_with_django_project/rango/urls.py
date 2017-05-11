a80e7ef1b2035fba1348c485adea93583a20e974
JeremiahZhang <z.l12@mail.scut.edu.cn>
2017-05-11 17:00:50 +0800

    :dancer: twd chapter 6 form

--- Tango-with-Django/tango_with_django_project/rango/urls.py
+++ Tango-with-Django/tango_with_django_project/rango/urls.py
@@ -8,4 +8,5 @@ urlpatterns = [
     url(r'^$', views.index, name='index'),
     url(r'^about/', views.about, name='about'), # '^rango/about/' here is '^about/'
     url(r'^category/(?P<category_name_slug>[\w\-]+)/$', views.show_category, name='show_category'),
+    url(r'^add_category/$', views.add_category, name='add_category'),
 ] # + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- Tango-with-Django/tango_with_django_project/rango/views.py
+++ Tango-with-Django/tango_with_django_project/rango/views.py
@@ -8,6 +8,8 @@ from django.http import HttpResponse
 from rango.models import Category
 from rango.models import Page
 
+from rango.forms import CategoryForm
+
 def index(request): # creat one view called indext
     # 传递到 templates/index.html 中的 html 变量
     # top five categories
@@ -52,3 +54,25 @@ def show_category(request, category_name_slug):
     
     # Go Render the response and return it to the client.
     return render(request, 'rango/category.html', context_dict)
+
+def add_category(request):
+    form = CategoryForm()
+    
+    # A HTTP POST?
+    if request.method == 'POST':
+        form = CategoryForm(request.POST)
+        
+        # Have we been provided with a valid form?
+        if form.is_valid():
+            # Save the new category to the database.
+            form.save(commit=True)
+            # Now that the category is saved.
+            # We could give a confirmation message
+            # But since the most recent category added is on the index page
+            # Then we can direct the user back to the index page.
+            return index(request)
+        else:
+            print(form.errors)
+    # Will handle the bad form, new form, or no form supplied cases.
+    # Render the form with error messages(if any).
+    return render(request, 'rango/add_category.html', {'form': form})
