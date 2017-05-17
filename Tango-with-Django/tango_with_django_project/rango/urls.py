59b834bd10a606f62d964dcd68abe2cd1eb1d230
JeremiahZhang <z.l12@mail.scut.edu.cn>
2017-05-17 15:07:55 +0800

    'twd-chapter6-exercise'

--- Tango-with-Django/tango_with_django_project/rango/urls.py
+++ Tango-with-Django/tango_with_django_project/rango/urls.py
@@ -8,5 +8,6 @@ urlpatterns = [
     url(r'^$', views.index, name='index'),
     url(r'^about/', views.about, name='about'), # '^rango/about/' here is '^about/'
     url(r'^category/(?P<category_name_slug>[\w\-]+)/$', views.show_category, name='show_category'),
+    url(r'^category/(?P<category_name_slug>[\w\-]+)/add_page/$', views.add_page, name='add_page'),
     url(r'^add_category/$', views.add_category, name='add_category'),
 ] # + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- Tango-with-Django/tango_with_django_project/rango/views.py
+++ Tango-with-Django/tango_with_django_project/rango/views.py
@@ -9,6 +9,7 @@ from rango.models import Category
 from rango.models import Page
 
 from rango.forms import CategoryForm
+from rango.forms import PageForm
 
 def index(request): # creat one view called indext
     # 传递到 templates/index.html 中的 html 变量
@@ -76,3 +77,26 @@ def add_category(request):
     # Will handle the bad form, new form, or no form supplied cases.
     # Render the form with error messages(if any).
     return render(request, 'rango/add_category.html', {'form': form})
+
+def add_page(request, category_name_slug):
+    try:
+        category = Category.objects.get(slug=category_name_slug)
+    except Category.DoesNotExist:
+        category = None
+    
+    form = PageForm
+    if request.method == "POST":
+        form = PageForm(request.POST)
+        if form.is_valid():
+            if category:
+                page = form.save(commit=False)
+                page.category = category
+                page.views = 0
+                page.save()
+                return show_category(request, category_name_slug)
+            else:
+                print(form.errors)
+        
+    context_dict = {'form': form, 'category': category}
+        
+    return render(request, 'rango/add_page.html', context_dict)
