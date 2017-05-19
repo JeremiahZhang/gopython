7df0f10f1947928a980922003587be5c2af444d1
JeremiahZhang <z.l12@mail.scut.edu.cn>
2017-05-19 11:22:15 +0800

    'lewis-project-setting'

--- cslewis/lewis/lewis/settings.py
+++ cslewis/lewis/lewis/settings.py
@@ -105,7 +105,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Asia/Shanghai'
 
 USE_I18N = True
 
@@ -118,3 +118,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
