07551ae5b5bd163d2036f43a8c919c62eb59fa60
JeremiahZhang <z.l12@mail.scut.edu.cn>
2017-05-24 14:51:39 +0800

    'js-image'

--- mdn-web/test-site/scripts/main.js
+++ mdn-web/test-site/scripts/main.js
@@ -1,2 +1,11 @@
-var myHeading = document.querySelector('h1');
-myHeading.textContent = 'Hello world!'
+var myImage = document.querySelector('img');
+
+myImage.onclick = function() {
+    var mySrc = myImage.getAttribute('src');
+    
+    if(mySrc == 'https://upload.wikimedia.org/wikipedia/en/thumb/1/1e/C.s.lewis3.JPG/220px-C.s.lewis3.JPG') {
+        myImage.setAttribute('src', 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRSGZG8cGsd888PCrePP4zckA4X2hUvPv4R3-5_fuhdc2T53QXX')
+    } else {
+        myImage.setAttribute('src', 'https://upload.wikimedia.org/wikipedia/en/thumb/1/1e/C.s.lewis3.JPG/220px-C.s.lewis3.JPG')
+    }
+}
