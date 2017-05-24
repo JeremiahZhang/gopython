5cb7d8961df0bd61d26f90a67440ac9ca4cc1051
JeremiahZhang <z.l12@mail.scut.edu.cn>
2017-05-24 15:00:03 +0800

    'more-js-src'

--- mdn-web/test-site/scripts/main.js
+++ mdn-web/test-site/scripts/main.js
@@ -9,3 +9,23 @@ myImage.onclick = function() {
         myImage.setAttribute('src', 'https://upload.wikimedia.org/wikipedia/en/thumb/1/1e/C.s.lewis3.JPG/220px-C.s.lewis3.JPG')
     }
 }
+
+var myButton = document.querySelector('button');
+var myHeading = document.querySelector('h1');
+
+function setUserName() {
+    var myName = prompt('Please enter your name.');
+    localStorage.setItem('name', myName);
+    myHeading.textContent = "C.S.Lewis was cool, " + myName;
+}
+
+if(!localStorage.getItem('name')) {
+    setUserName();
+} else {
+    var storedName = localStorage.getItem('name');
+    myHeading.textContent = 'C.S.Lewis was coll, ' + storedName;
+}
+
+myButton.onclick = function() {
+    setUserName();
+}
