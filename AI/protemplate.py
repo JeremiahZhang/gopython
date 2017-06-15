087802f4fa1f90519f8950f5e6d98d6b96ddeef8
JeremiahZhang <z.l12@mail.scut.edu.cn>
2017-06-15 11:31:22 +0800

    'template-upd'

--- AI/protemplate.py
+++ AI/protemplate.py
@@ -1,7 +1,38 @@
 # Python Project Template
 # 1. Prepare Problem
 # a) Load libraries
+
+from pandas import read_csv
+from pandas.tools.plotting import scatter_matrix
+
+import matplotlib.pyplot as plt
+
+from sklearn.model_selection import train_test_split
+from sklearn.model_selection import KFold
+from sklearn.model_selection import cross_val_score
+
+from sklearn.metrics import classification_report
+from sklearn.metrics import confusion_matrix
+from sklearn.metrics import accuracy_score
+
+from sklearn.linear_model import LogisticRegression
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.discriminant_analysis import LinearDiscriminantAnalysis
+from sklearn.naive_bayes import GaussianNB
+from sklearn.svm import SVC
+
 # b) Load dataset
+# Load dataset using url
+url = "https://archive.ics.uci.edu/ml/machine-learning-databases/iris/iris.data"
+names = ['sepal-length', 'sepal-width', 'petal-lenght', 'petal-width', 'class']
+df = read_csv(url, names=names)
+
+# Load local dataset 
+# filename = 'iris.data.csv'
+# names = ['sepal-length', 'sepal-width', 'petal-lenght', 'petal-width', 'class']
+# df = read_csv(filename, names=names)
+
 # 2. Summarize Data
 # a) Descriptive statistics
 # b) Data visualizations
