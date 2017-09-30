fd83cc6423e3d6adaae46eb391a6eaac5e58bf92
Jeremy Anifacc <zhangleisuda@gmail.com>
2017-09-30 11:15:06 +0800

    ex_7

--- Python-Programming-an-Introduction-to-CS/src/ch01/chaos.py
+++ Python-Programming-an-Introduction-to-CS/src/ch01/chaos.py
@@ -4,8 +4,14 @@
 def main():
     print("This program illustrates a chaotic function.")
     x = eval(str(input("Enter a number between 0 and 1: ")))
-    for i in range(10):
-        x = 3.9 * x * (1 - x)
+    for i in range(20):
+        x = 2.0 * x * (1 - x)
         print(x)
 
-main()
+def ex_7():
+    for i in range(2):
+        x = eval(str(input("Enter a number between 0 and 1: ")))
+
+        for ii in range(20):
+            x = 3.9 * x * (1 - x)
+            print(x)
