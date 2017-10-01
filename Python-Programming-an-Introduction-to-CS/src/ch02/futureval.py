f1747d843b58552e8602165df95aec8048705e5e
JeremiahZhang <zhangleisuda@gmail.com>
2017-10-01 23:43:50 +0800

    ch02_ex06 compound rate magic

--- Python-Programming-an-Introduction-to-CS/src/ch02/futureval.py
+++ Python-Programming-an-Introduction-to-CS/src/ch02/futureval.py
@@ -26,4 +26,22 @@ def main2():
 
     print("The value in {0} years is: {1}.".format(num_year, principal))
 
-main2()
+# exercise 6
+def main3():
+    print("This program calculates the total accumulation of")
+    print("your investment, in the condition that you invest a certain fixed amount")
+    print("money every years.")
+
+    # The money you want to invest every year
+    print("Please　enter principal of each year and rate, seperate by comma:")
+    annual_principal, apr = eval(input())
+
+    print("How many years do you want to invest?")
+    num_year = eval(input("Please enter the number of years: "))
+
+    total_accumulation = annual_principal * (1 + apr) * ((1 + apr)**num_year -1) / apr
+    print("The total_accumulation of the investment: {}".format(total_accumulation))
+
+# main()
+# main2()
+main3()
