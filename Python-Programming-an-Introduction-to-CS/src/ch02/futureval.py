04adcc19b293c0bfb57e00eae4fd1f2ae235e9e6
JeremiahZhang <zhangleisuda@gmail.com>
2017-10-01 16:28:24 +0800

    ch02_ex05

--- Python-Programming-an-Introduction-to-CS/src/ch02/futureval.py
+++ Python-Programming-an-Introduction-to-CS/src/ch02/futureval.py
@@ -14,4 +14,16 @@ def main():
 
     print("The value in 10 years is: {}".format(principal))
 
-main()
+# exercise 5
+def main2():
+    print("This program calculates the future value")
+    print("of an investment, that the year is defined by the user")
+
+    principal, apr = eval(input("Please enter principal and rate, seperated by comma: "))
+    num_year = eval(input("Please input the year you want to invest: "))
+
+    principal = principal * (1 + apr) ** num_year
+
+    print("The value in {0} years is: {1}.".format(num_year, principal))
+
+main2()
