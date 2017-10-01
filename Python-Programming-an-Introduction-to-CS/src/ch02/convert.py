18b7f6c59d628ae54c070d7e72e438591e858c29
JeremiahZhang <zhangleisuda@gmail.com>
2017-10-01 15:25:17 +0800

    ch01 pro_ex_01

--- Python-Programming-an-Introduction-to-CS/src/ch02/convert.py
+++ Python-Programming-an-Introduction-to-CS/src/ch02/convert.py
@@ -5,6 +5,9 @@
 # by Jeremy Anifacc
 
 def main():
+    print("This is a program to convert ")
+    print("Celsius temperature to Fahrenheit.")
+
     celsius = float(input("What is the Cesisus temperature? Enter a number _> "))
     fahrenheit = 9.00 / 5.00  * celsius + 32.00
     print("The temperature is {0:.2f} degrees Fahrenheit.".format(fahrenheit))
