205a3c0f803e5c9ca2d6a0700327be0e4c887eb1
Jeremy Anifacc <zhangleisuda@gmail.com>
2017-10-27 09:33:31 +0800

    'mod'

--- AI/course-learning-from-data-of-caltech/hw5_questions/logistic_regression.py
+++ AI/course-learning-from-data-of-caltech/hw5_questions/logistic_regression.py
@@ -13,21 +13,18 @@ def target(feature, p1, p2):
     x2, y2 = p2
     scope = (y2 - y1)/(x2 - x1)
 
-    return 1 if y > (scope * (x - x1) + y1) else -1
+    return 1 if y >= (scope * (x - x1) + y1) else -1
 
 def cross_entropy(feature, label, weights):
     error = math.log(1 + math.exp(-label*np.dot(feature, weights)))
-    return error
-
-def gradient(feature, label, weights):
-    num = -np.multiply(feature, label)
-    return num / (1 + math.exp(label * np.dot(weights, feature)))
+    error_diff = np.array(-label * feature) / (1 + math.exp(label * np.dot(weights, feature)))
+    return error, error_diff
 
 def training(size, weights, p1, p2):
-    eta = 0.01
+    eta = 0.01 # learning rate
 
     training_set = gen_dataset(size)
-    true_label = [target(point, p1, p2) for point in training_set]
+    true_label = np.array([[target(point, p1, p2)] for point in training_set])
 
     t = 0
     previous_weights = weights
@@ -35,17 +32,23 @@ def training(size, weights, p1, p2):
     distance = 1
 
     while not distance < 0.01:
-        idx = random.randint(0, size-1)
-        x_feature = np.array(training_set[idx])
-        x_label = true_label[idx]
+        # idx = random.randint(0, size-1)
+        # x_feature = np.array(training_set[idx])
+        # x_label = true_label[idx]
 
-        descent = gradient(x_feature, x_label, weights)
-        weights = previous_weights - eta * descent
+        permuted_order = range(size)
+        random.shuffle(permuted_order)
 
-        distance = np.linalg.norm(previous_weights - weights)
-        previous_weights = weights
+        for idx in permuted_order:
+            x_feature = np.array(training_set[idx])
+            x_label = true_label[idx]
 
-        t += 1
+            error, descent = cross_entropy(x_feature, x_label, weights)
+            weights = previous_weights - eta * descent
+
+            t += 1
+            distance = np.linalg.norm(previous_weights - weights)
+            previous_weights = weights
 
     return t, previous_weights
 
@@ -56,7 +59,8 @@ def testing(size, weights, p1, p2):
 
     for feature in testing_set:
         true_label = target(feature, p1, p2)
-        e_out += cross_entropy(feature, true_label, weights)
+        error, descent = cross_entropy(feature, true_label, weights)
+        e_out += error
 
     e_out = e_out / float(size)
     return e_out
@@ -65,15 +69,14 @@ def main():
     p1 = (random.uniform(-1, 1), random.uniform(-1, 1))
     p2 = (random.uniform(-1, 1), random.uniform(-1, 1))
 
-    weights = [0., 0., 0]
     N = 100
     training_size = 100
-    testing_size = 100
+    testing_size = 1000
 
     avg_iters = 0.0
     avg_eout = 0.0
 
-    weights = np.array([0., 0., 0.])
+    weights = np.array([0, 0, 0])
 
     for i in xrange(N):
         iterations, final_weights = training(training_size, weights, p1, p2)
@@ -91,8 +94,6 @@ if __name__ == '__main__':
     main()
 
 """
-python logistic_regression.py
-Average iterations: 1.0
-Average error out of sample: 0.692341732055
-There are some problems
+Average iterations: 100.0
+Average error out of sample: 0.584978759685
 """
