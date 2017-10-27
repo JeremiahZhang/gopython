8f4819015c42785ce58e338c956a4132af64c587
Jeremy Anifacc <zhangleisuda@gmail.com>
2017-10-27 16:31:15 +0800

    'regression-prediction-plot'

--- AI/practice-machine-learning-with-python/best_fit.py
+++ AI/practice-machine-learning-with-python/best_fit.py
@@ -16,12 +16,26 @@ def main():
     m, b = best_fit_slope(xs, ys)
     print("Best fit slope: {} and constant b: {}".format(m, b))
 
+    # plot regression_line
     regression_line = [(m*x) + b for x in xs]
 
-    style.use('ggplot')
+    plt.figure(1)
     plt.scatter(xs, ys, color="#003F72")
     plt.plot(xs, regression_line)
     plt.show()
+
+    # prediction
+    predict_x = 7
+    predict_y = (m*predict_x) + b
+    print("Prediction of x({}) is {}".format(predict_x, predict_y))
+
+    plt.figure(2)
+    plt.scatter(xs, ys, color="#003F72", label="data")
+    plt.plot(xs, regression_line, label="regression line")
+    plt.scatter(predict_x, predict_y, color="green", label="predict_data")
+    plt.legend(loc=4)
+    plt.show()
+
 
 if __name__ == '__main__':
     main()
