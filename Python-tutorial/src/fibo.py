3b9ae9ba8db859cf2c5bbf0eb3d84e9a43d305ed
JeremiahZhang <zhangleisuda@gmail.com>
2017-11-20 19:49:49 +0800

    simple module

---
+++ Python-tutorial/src/fibo.py
@@ -0,0 +1,32 @@
+# Fibonacci numbers module
+
+def fib(n):    # write Fibonacci series up to n
+    a, b = 0, 1
+    while b < n:
+        print b,
+        a, b = b, a+b
+
+def fib2(n):  # return Fibonacci series up to n
+    result = []
+    a, b = 0, 1
+    while b < n:
+        result.append(b)
+        a, b = b, a+b
+
+    return result
+
+"""
+>>> import fibo
+>>> fibo.fib(1000)
+1 1 2 3 5 8 13 21 34 55 89 144 233 377 610 987
+>>> fibo.fib2(1000)
+[1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144, 233, 377, 610, 987]
+>>> fibo.__name__
+'fibo'
+>>> # if you intend to use a function often you can assign it to a local name:
+...
+>>> fib = fibo.fib
+>>> fib(500)
+1 1 2 3 5 8 13 21 34 55 89 144 233 377
+>>>
+"""
