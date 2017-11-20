4afa937143592d252c511d7e7a1db140763b3bb0
JeremiahZhang <zhangleisuda@gmail.com>
2017-11-20 20:14:06 +0800

    excute modules as script 6.1.1

--- Python-tutorial/src/fibo.py
+++ Python-tutorial/src/fibo.py
@@ -15,6 +15,13 @@ def fib2(n):  # return Fibonacci series up to n
 
     return result
 
+def main():
+    import sys
+    fib(int(sys.argv[1]))
+
+if __name__ == '__main__':
+    main()
+
 """
 >>> import fibo
 >>> fibo.fib(1000)
