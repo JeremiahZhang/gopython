f25510cafa3fad23f3127978796e0bf639496a81
jeremyzhang <zhangleisuda@gmail.com>
2018-07-28 17:16:54 +0800

    copy img from url

---
+++ python4everybody/ch12_network_programming/curl2.py
@@ -0,0 +1,15 @@
+import urllib.request, urllib.parse, urllib.error
+
+img = urllib.request.urlopen('http://data.pr4e.org/cover3.jpg')
+
+print('Writing')
+fhand = open('cover3.jpg', 'wb')
+size = 0
+while True:
+    info = img.read(10000)
+    if len(info) < 1: break
+    size = size + len(info)
+    fhand.write(info)
+
+print(size, 'characters copied.')
+fhand.close()
