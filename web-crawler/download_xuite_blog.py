f041c1793bbdfa02a21a187180ebd7aa7b6c9493
jeremiahzhang <zhangleisuda@gmail.com>
2019-03-11 23:30:40 +0800

    xuite_blog scrapy 2.0

--- web-crawler/download_xuite_blog.py
+++ web-crawler/download_xuite_blog.py
@@ -5,11 +5,10 @@ import requests, os, bs4
 
 url = 'https://blog.xuite.net/venusvogue99/twblog'
 os.makedirs('xuite_blog', exist_ok=True)
-endstr = 0
-n = 0
+end_str = ' '
 
 # while not url.endswith('287'):
-while n < 2:
+while not url.endswith(end_str):
     # Todo: Download the page.
     print('Downloading articles %s ...' % url)
     res = requests.get(url)
@@ -36,6 +35,8 @@ while n < 2:
             article_soup = bs4.BeautifulSoup(res.text, features='lxml')
             # Get single article title name 
             article_titlename = article_soup.select('.titlename')[0].getText()
+            # Del special characters
+            article_titlename = ''.join(e for e in article_titlename if e.isalnum())
             print(article_titlename)
             # TODO: Save the single article as html format to ./xuite_blog
             html_file = open(os.path.join('xuite_blog', os.path.basename(article_url) + article_titlename
@@ -48,15 +49,15 @@ while n < 2:
         # Todo: Get the Prev button's url
         # Go to next page to get another ten articles
         page_link = soup.select('.page a')
+        # Next page 
         prev_link = page_link[len(page_link)-2]
+        # Last article page 
         last_link = page_link[len(page_link)-1]
-        # print(prev_link)
+
         url = 'https:' + prev_link.get('href')
-        # print(url)
         last_url  = last_link.get('href')
+
         end_str = os.path.basename(last_url)
         print(end_str)
-
-    n += 1
 
 print('Done.')
