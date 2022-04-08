0067a0ed4d4a848a47f5ed0f6837d996d17cc8ab
JeremyAnifacc <zhangleisuda@gmail.com>
2022-04-08 21:23:19 +0800

    add question

--- python-daily/hunderd_challenging_01.py
+++ python-daily/hunderd_challenging_01.py
@@ -1,4 +1,13 @@
 def find_number():
+	""" 
+	Question:
+		Write a program which will find all 
+		such numbers which are divisible by 7 but 
+		are not a multiple of 5,
+		between 2000 and 3200 (both included).
+		The numbers obtained should be printed 
+		in a comma-separated sequence on a single line.
+	"""
 	lst = []
 	for i in range(2000, 3201):
 		if ((i%7) == 0) & ((i%5) != 0):
