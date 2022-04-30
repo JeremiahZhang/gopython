8cebcc253b34021c30255d071e3689709c5a9e6e
JeremyAnifacc <zhangleisuda@gmail.com>
2022-04-30 16:06:00 +0800

    to_binary

--- python-daily/decimal_to_binary.py
+++ python-daily/decimal_to_binary.py
@@ -1,13 +1,17 @@
 def to_binary(num):
-	if num >= 1:
+	if num == 0:
+		return(1)
+
+	if num > 1:
 		to_binary(num // 2)
+
 
 	# remainder
 	print(num % 2)
 
 def main():
 	n = int(input())
-	to_binary
+	to_binary(n)
 
 if __name__ == '__main__':
 	main()
