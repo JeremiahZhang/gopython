e41c9fd18d0bf65d9795f7c92b39f100da7db8b8
JeremyAnifacc <zhangleisuda@gmail.com>
2022-05-02 21:05:37 +0800

    set unique

--- practical-python/container.py
+++ practical-python/container.py
@@ -17,5 +17,35 @@ prices = {
 	'ibm': 92.12,
 	'msft': 44.21
 }
-
 print("price of ibm is {}".format(prices['ibm']))
+
+# Composite keys
+
+holidays = {
+	(1, 1): 'New Years',
+	(3, 14): 'Pi day',
+	(9, 13): "Programmer's day",
+}
+
+print(holidays[3, 14])
+
+# set
+tech_stocks1 = {'IBM', 'AAPL', 'MSFT'}
+tech_stocks2 = set(['IBN', 'AAPL', 'MSFT'])
+
+print(tech_stocks1)
+print(tech_stocks2)
+
+if 'IBM' in tech_stocks1:
+	print("Yes, {} in the stocks repository".format('IBM'))
+
+print('FB' in tech_stocks1)
+
+# unique
+names = ['IBM', 'AAPL', 'GOOG', 'IBM', 'GOOG', 'YHOO']
+unique = set(names)
+
+unique.add('CAT')
+unique.add('META')
+unique.remove('YHOO')
+print(type(unique))
